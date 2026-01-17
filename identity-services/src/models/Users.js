75fd955e2944dd65548304b3916bcf7a2fdcaed6
6829nkhpas <parnamnkh1@gmail.com>
2026-01-18 00:27:10 +0530

    user model with verfication and indexing

---
+++ identity-services/src/models/Users.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+const argon2 = require('argon2');
+
+const userschema = new mongoose.Schema({
+    username:{
+        type: String,
+        unique:true,
+        trim:true,
+        required: true
+    },
+    email:{
+        type: String,
+        unique:true,
+        required:true,
+        trim:true,
+        lowercase:true
+    },
+    password:{
+        type: String,
+        required:true,
+        trim:true,
+
+    },
+    createdAt:{
+        type: Date,
+        default:Date.now
+    }
+
+},
+{
+    timestamps: true
+});
+
+userschema.pre('save', async function(next){
+    if(this.isModified('password')){
+       try {
+         this.password = await argon2.hash(this.password)
+       } catch (error) {
+        return next(error);
+        
+       }
+    }
+  
+})
+userschema.methods.comparePassword = async function (candidatePasword) {
+    try {
+        return await argon2.verify(thi.password , candidatePasword)
+    } catch (error) {
+        throw error
+        
+    }
+    
+}
+userschema.index({username: 'text'});
+ 
+const User = mongoose.model('User', userschema);    
+
+module.exports = User;
