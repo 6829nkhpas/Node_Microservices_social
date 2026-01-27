be0fca5051b80850aa4a8351f09fa03c8925d6cd
6829nkhpas <parnamnkh1@gmail.com>
2026-01-28 01:40:55 +0530

    fix:password expressionn change

--- identity-services/src/utils/Validation.js
+++ identity-services/src/utils/Validation.js
@@ -6,7 +6,7 @@ const validationcheck =(data)=>{
     const schema = joi.object({
         username: joi.string().min(3).max(50).required(),
         email: joi.string().email().required(),
-        password: joi.string().min(6).max(50).pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
+        password: joi.string().min(6).max(50).required(),
 
     })
     return schema.validate(data);
